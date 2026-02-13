7c46f675f2e954b6b297dd52544f64b40b8cd2a8
NoloxoYT <noloxoyt@gmail.com>
2026-02-13 14:10:21 +0000

    massive

--- README.md
+++
@@ -1,2 +0,0 @@
-# B-Qol
-A Fabric mod for Minecraft Java Edition adding quality of life features such as Ctrl+Z (undo), Ctrl+N (notepad), and more.
---
+++ src/client/java/com/noloxo/bqol/BQolDataGenerator.java
@@ -0,0 +1,11 @@
+package com.noloxo.bqol;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BQolDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
