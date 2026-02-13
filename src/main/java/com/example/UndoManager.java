7942b782681514f4b94c94f97b021dae775147e8
NoloxoYT <noloxoyt@gmail.com>
2026-02-13 15:14:31 +0100

    Add UndoManager class

---
+++ src/main/java/com/example/UndoManager.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import java.util.Stack;
+
+public class UndoManager {
+    private static final Stack<UndoAction> undoStack = new Stack<>();
+
+    public static void pushAction(BlockPos pos, BlockState oldState, BlockState newState, ServerWorld world) {
+        undoStack.push(new UndoAction(pos, oldState, newState, world));
+    }
+
+    public static void undoLastAction() {
+        if (!undoStack.isEmpty()) {
+            UndoAction action = undoStack.pop();
+            action.undo();
+        }
+    }
+
+    public static boolean canUndo() {
+        return !undoStack.isEmpty();
+    }
+
+    private static class UndoAction {
+        private final BlockPos pos;
+        private final BlockState oldState;
+        private final BlockState newState;
+        private final ServerWorld world;
+
+        public UndoAction(BlockPos pos, BlockState oldState, BlockState newState, ServerWorld world) {
+            this.pos = pos;
+            this.oldState = oldState;
+            this.newState = newState;
+            this.world = world;
+        }
+
+        public void undo() {
+            world.setBlockState(pos, oldState, 3);
+        }
+    }
+}
